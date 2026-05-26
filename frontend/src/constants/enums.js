6c8a164c9e9b59c2516773afebed4751754968fe
Viraj Sheoran <viraj.sheoran10@gmail.com>
2026-05-27 01:49:12 +0530

    chore: add frontend enums constants

---
+++ frontend/src/constants/enums.js
@@ -0,0 +1,13 @@
+export const BRANCHES = Object.freeze([
+  'Artificial Intelligence and Machine Learning',
+  'Electronics and Telecommunication',
+  'Computer Science',
+  'Robotics and Automation',
+  'Mechanical Engineering',
+  'Civil Engineering',
+]);
+
+export const STUDENT_GENDERS = Object.freeze([
+  { value: 'male', label: 'Male' },
+  { value: 'female', label: 'Female' },
+]);
